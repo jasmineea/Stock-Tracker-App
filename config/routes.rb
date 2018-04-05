3d5f7628e88ee8f0e033bbbd5c3589fa2118caf4
Jasmine <jasmineanderson315@yahoo.com>
2018-04-04 21:24:09 -0700

    ready for production

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
  devise_for :users, :controllers => { :registrations => "user/registrations" }
-  root to: "welcome#index"
-  get 'my_portfolio', to: "users#my_portfolio"
+  root 'welcome#index'
+  get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
-  get 'my_friends', to: 'users#my_friends'
-  get 'search_friends', to: 'users#search'
   resources :user_stocks, only: [:create, :destroy]
   resources :users, only: [:show]
   resources :friendships
-
+  get 'my_friends', to: 'users#my_friends'
+  get 'search_friends', to: 'users#search'
+  post 'add_friend', to: "users#add_friend"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
